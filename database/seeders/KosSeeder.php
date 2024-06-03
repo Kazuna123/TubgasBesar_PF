5b7ff5f3563cdf300e11a9533c0182de102b7f6f
unknown <kazuma200300@gmail.com>
2024-06-03 16:49:13 +0700

    first commit

---
+++ database/seeders/KosSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kos extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'luas_id',
+        'tipe_id',
+        'slug',
+        'nama',
+        'harga',
+        'alamat',
+        'fasilitas',
+        'jarak',
+        'gambar'
+    ];
+
+    public function gambars()
+    {
+        return $this->hasMany(Gambar::class);
+    }
+}
